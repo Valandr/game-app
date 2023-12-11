a910da13c53888d0aa5eeee2e414f042ad4109aa
Valandr <valerie.andriani@gmail.com>
2023-12-11 14:32:53 +0100

    Sorting games

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,14 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
+  function onSelectSortOrder(order: string): void {
+    throw new Error("Function not implemented.");
+  }
 
   return (
     <Grid
@@ -45,7 +50,12 @@ function App() {
               setGameQuery({ ...gameQuery, platform })
             }
           />
-          <SortSelector />
+          <SortSelector
+            sortOrder={gameQuery.sortOrder}
+            onSelectSortOrder={(sortOrder) =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
+          />
         </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
